db1f40b401af1d7ddd3b30c4389aeff6331753f3
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-08-07 18:04:36 +0500

    second commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,46 @@
 import axios from 'axios'
+import { Password } from 'primereact/password'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { InputOtp } from 'primereact/inputotp';
+import OTPInput from 'react-otp-input';
+import { Button, Input, InputGroup, InputRightElement, useToast } from '@chakra-ui/react';
 
 const Home = () => {
   const [state, setState] = useState([])
   const navigate = useNavigate()
+  const [password, setPassword] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [show, setShow] = React.useState(false)
+  const handleClick = () => setShow(!show)
+  const toast = useToast()
+
+  const correctPassword = 'Begov__1999';
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    if (password === correctPassword) {
+      setIsAuthenticated(true);
+      localStorage.setItem('isAuthenticated', 'true');
+    } else {
+      toast({
+        title: 'Incorrect Password!',
+        description: "Please refill the password input and try again",
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
+      return;
+    }
+
+    setPassword("")
+  };
+
+  const handleLogOut = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('isAuthenticated');
+  }
 
   const getData = async () => {
     try {
@@ -19,22 +55,57 @@ const Home = () => {
     getData()
   }, [])
 
+  useEffect(() => {
+    const auth = localStorage.getItem('isAuthenticated');
+    if (auth === 'true') {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
-    <div className='flex items-center justify-center w-full h-screen'>
-      <div className='grid gap-10'>
-        <h1 className='text-5xl text-blue-600 text-center font-bold'>Choose the group</h1>
-        <div className='max-sm:flex max-sm:justify-center'>
-          <div className='grid grid-cols-3 justify-items-center gap-5 max-sm:grid-cols-2'>
-            {state.map((product) => {
-              return (
-                <div onClick={() => navigate(`/${product.href}`)} key={product.id} className='cursor-pointer hover:bg-blue-700 hover:shadow-md hover:shadow-blue-600 transition-all px-5 py-3 max-sm:flex max-sm:justify-center rounded-xl bg-blue-600'>
-                  <h1 className='text-lg font-medium text-white'>{product.name}</h1>
-                </div>
-              )
-            })}
+
+    <div>
+      {isAuthenticated ? (
+        <div className='flex items-center justify-center w-full h-screen'>
+          <div className='grid gap-10'>
+            <div className='flex items-center justify-center'>
+              <button onClick={handleLogOut} className='text-md text-white text-center cursor-pointer px-2 py-1 rounded-md bg-red-500 w-fit'>Log Out</button>
+            </div>
+            <h1 className='text-5xl text-blue-600 text-center font-bold'>Choose the group</h1>
+            <div className='max-sm:flex max-sm:justify-center'>
+              <div className='grid grid-cols-3 justify-items-center gap-5 max-sm:grid-cols-2'>
+                {state.map((product) => {
+                  return (
+                    <div onClick={() => navigate(`/${product.href}`)} key={product.id} className='cursor-pointer hover:bg-blue-700 hover:shadow-md hover:shadow-blue-600 transition-all px-5 py-3 max-sm:flex max-sm:justify-center rounded-xl bg-blue-600'>
+                      <h1 className='text-lg font-medium text-white'>{product.name}</h1>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className='flex items-center justify-evenly w-full h-screen max-w-[1250px] mx-auto'>
+          <form onSubmit={handleLogin} className='w-full flex items-center justify-center flex-col gap-5'>
+            <InputGroup size='lg' w={'50%'}>
+              <Input
+                pr='4.5rem'
+                type={show ? 'text' : 'password'}
+                placeholder='Enter password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width='4.5rem'>
+                <Button h='1.75rem' size='sm' onClick={handleClick}>
+                  {show ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Button colorScheme='blue' w={'8%'}>Enter!</Button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { PrimeReactProvider } from 'primereact/api'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <PrimeReactProvider>
+          <App />
+        </PrimeReactProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
